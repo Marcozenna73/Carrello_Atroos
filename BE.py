cfc078f65142ae30daaba9f79dcbbd2d575042f5
Marcozenna73 <83497086+Marcozenna73@users.noreply.github.com>
2023-03-31 14:41:09 +0200

    Add files via upload

---
+++ BE.py
@@ -0,0 +1,8 @@
+import urllib.request
+import webbrowser
+
+with urllib.request.urlopen('file:///C:/Users/marco/Desktop/Progetto%20lavoro/FE.html') as response: 
+    html = response.read()
+#print(html)
+
+webbrowser.open('file:///C:/Users/marco/Desktop/Progetto%20lavoro/FE.html')
