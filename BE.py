0697b2da65166b859e9aefd1f810c28ae76c317a
Marcozenna73 <marco.zennaro73@gmail.com>
2023-04-07 02:10:20 +0200

    Requisiti obbligatori

--- BE.py
+++ BE.py
@@ -1,8 +1,8 @@
 #import urllib.request
-import webbrowser
+#import webbrowser
 
 #with urllib.request.urlopen('file:///C:/Users/marco/Desktop/Progetto%20lavoro/FE.html') as response: 
 #    html = response.read()
 #print(html)
 
-webbrowser.open('file:///C:/Users/marco/Desktop/Progetto%20lavoro/Carrello_Atroos/templates/PaginaHome.html')
+#webbrowser.open('file:///C:/Users/marco/Desktop/Progetto%20lavoro/Carrello_Atroos/templates/PaginaHome.html')
--- Backend.py
+++ Backend.py
@@ -1,6 +1,7 @@
+import json
 import mysql.connector
 from mysql.connector import Error
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 
 def create_db_connection(host_name, user_name, user_password, db_name):
@@ -37,9 +38,33 @@ def read_query(connection, query):
         print(f"Error: '{err}'")
 
 def mostra_articoli_carrello(utente, mostra_articoli_utente):
-    query = mostra_articoli_utente + utente
+    query = mostra_articoli_utente + str(utente)
     articoli = read_query(connection, query)
     return articoli
+
+def acquista_articoli(utente, art_ord):
+    acquista_art1 = """
+    UPDATE articles 
+    SET Quantita_disponibile = """
+    acquista_art2 = """ 
+    WHERE Nome = '"""
+    effettua_ord = """
+    UPDATE orders 
+    SET Stato = 'Settled' 
+    WHERE Utente = """
+    for art in art_ord:
+        quantita_art = art[3] - art[2]
+        query_acquisto = acquista_art1 + str(quantita_art) + acquista_art2 + art[0] + "'"
+        print(query_acquisto)
+        execute_query(connection, query_acquisto)
+    query_ordine = effettua_ord + str(utente)
+    print(query_ordine)
+    execute_query(connection, query_ordine)
+
+def svuotaCarr(ordine):
+    query_svuota = svuota_car + str(ordine)
+    print(query_svuota)
+    execute_query(connection, query_svuota)
 
 connection = create_db_connection("localhost", "root", "", "carrello atroos")
 
@@ -56,13 +81,15 @@ INSERT INTO users(Nome, Cognome, CF) VALUES
 
 inserimento_articoli_in_ordini = """
 INSERT INTO contiene(Ordine, Articolo, Quantità) VALUES
-(1, 'Pasta', 5);
+(1, 'Pasta', 5),
+(1, 'Pizza', 3),
+(1, 'Pane', 7);
 """
 
-svuota = """
+svuota_car = """
 DELETE 
-FROM users
-"""
+FROM contiene
+WHERE Ordine = """
 
 mostra_tutto = """
 SELECT * 
@@ -70,7 +97,7 @@ FROM contiene
 """
 
 mostra_articoli = """
-SELECT Nome, Prezzo 
+SELECT Nome, Prezzo, Foto 
 FROM articles
 """
 
@@ -79,37 +106,75 @@ SELECT orders.ID
 FROM orders NATURAL JOIN users"""
 
 mostra_articoli_utente = """
-SELECT Nome, Prezzo, Quantità 
+SELECT Nome, Prezzo, Quantità, Quantita_disponibile, Foto 
 FROM articles INNER JOIN contiene on articles.Nome = contiene.Articolo 
 INNER JOIN orders on orders.ID = contiene.Ordine
 WHERE orders.Stato = 'Pending' and orders.Utente = """
+
+mostra_utenti = """
+SELECT Nome 
+FROM users """
 
 #execute_query(connection, inserimento_articoli_in_ordini)
 #results = read_query(connection, mostra_tutto)
 #for result in results:
 #    print(result)
 
-articoli_ordine = mostra_articoli_carrello('1', mostra_articoli_utente)
+articoli_ordine = mostra_articoli_carrello(1, mostra_articoli_utente)
 totale_ordine = 0
 for articolo in articoli_ordine:
     totale_ordine += articolo[1]*articolo[2]
     print(articolo[0], end=' ')
     print(articolo[1], end=' ')
-    print(articolo[2])
+    print(articolo[2], end=' ')
+    print(articolo[3])
 
 numero_art_ord = len(articoli_ordine)
 
 articoli_negozio = read_query(connection, mostra_articoli)
 
-
+utenti_db = read_query(connection, mostra_utenti)
 
 @app.route('/')
-def nome_articolo():
-   return render_template('PaginaHome.html', articoli = articoli_negozio)
+def Home():
+   return render_template('PaginaHome.html', articoli = articoli_negozio, utenti = utenti_db)
 
 @app.route('/Carrello')
 def Carrello():
-    return render_template('PaginaCarrello.html', num_art_ord = numero_art_ord, articoli_ord = articoli_ordine, totale = totale_ordine)
+    articoli_ordine = mostra_articoli_carrello(1, mostra_articoli_utente)
+    totale_ordine = 0
+    for articolo in articoli_ordine:
+        totale_ordine += articolo[1]*articolo[2]
+    numero_art_ord = len(articoli_ordine)
+    return render_template('PaginaCarrello.html', num_art_ord = numero_art_ord, articoli_ord = articoli_ordine, totale = totale_ordine, utenti = utenti_db)
+
+@app.route('/acquista')
+def acquista():
+    articoli_ordine = mostra_articoli_carrello(1, mostra_articoli_utente)
+    for articolo in articoli_ordine:
+        if(articolo[2] >= articolo[3]):
+            return Carrello()
+    acquista_articoli(1, articoli_ordine)
+    return Carrello()
+
+@app.route('/svuotaCarrello')
+def svuotaCarrello():
+    svuotaCarr(1)
+    return Carrello()
+
+        
+#execute_query(connection, inserimento_articoli_in_ordini)
+
+#@app.route('/Hello')
+#def Hello():
+#    return render_template('Hello.html', nome = 'Beatrice')
+
+#@app.route('/Pippo', methods=['GET', 'POST'])
+#def Pippo():
+#    print("Stampa")
+#    data = json.loads(request.data)
+#    print(data)
+#    return data
 
 if __name__ == '__main__':
    app.run()
